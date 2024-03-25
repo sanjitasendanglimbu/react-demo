c0fc97808c8f29ca730d72c0de52589174607185
sanjitasendanglimbu <sanjitasendang12@gmail.com>
2024-03-25 12:37:52 +0545

    this practice react

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <h1 className='bg-green-400 p-4 rounded-xl'>Tailwaind Test</h1>
+    <Card username="Manila" btnText="Hire Me"/>
+    <Card username="Harry" btnText="Add Me"/>
+
+    </>
+  )
+}
+
+export default App
---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <>
+     <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button onClick={()=> setColor("red")}className="outline-none px-4 rounded-full shadow-sm text-white" style={{backgroundColor: "red"}}>Red</button>
+          <button onClick={()=> setColor("blue")} className="outline-none px-4 rounded-full shadow-sm text-white" style={{backgroundColor: "blue"}}>Red</button>
+          <button onClick={()=> setColor("yellow")}className="outline-none px-4 rounded-full shadow-sm text-white" style={{backgroundColor: "yellow"}}>Red</button>
+          <button onClick={()=> setColor("green")} className="outline-none px-4 rounded-full shadow-sm text-white" style={{backgroundColor: "green"}}>Red</button>
+        </div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
